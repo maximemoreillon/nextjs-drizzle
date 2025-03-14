fb71eae65c5e79f0a0180d29166c57d85b454bfb
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-14 11:50:08 +0900

    pagination

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -3,6 +3,6 @@ import { readItems } from "@/app/actions";
 // TODO: POST
 
 export async function GET() {
-  const items = await readItems();
+  const items = await readItems({});
   return Response.json(items);
 }
