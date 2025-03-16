be8e7f02dfbb46f10a21d68c68e7bed2bc2ba28a
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-17 07:38:32 +0900

    more route handlers

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -1,10 +1,29 @@
-import { readItem } from "@/app/actions";
+import { deleteItem, readItem, updateItem } from "@/app/actions";
 
 export async function GET(
-  request: Request,
+  _: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   const garment = await readItem(Number(id));
   return Response.json(garment);
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const properties = await request.json();
+  await updateItem(Number(id), properties);
+  return Response.json({ id });
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  await deleteItem(Number(id));
+  return Response.json({ id });
 }
