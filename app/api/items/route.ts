0e08789f40efb7fa8f44cb23081576b9118a8056
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-16 09:40:01 +0900

    POST route handler

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,6 +1,10 @@
-import { readItems } from "@/app/actions";
+import { createItem, readItems } from "@/app/actions";
 
-// TODO: POST
+export async function POST(request: Request) {
+  const properties = await request.json();
+  const newGarment = await createItem(properties);
+  return Response.json(newGarment);
+}
 
 export async function GET() {
   const items = await readItems({});
